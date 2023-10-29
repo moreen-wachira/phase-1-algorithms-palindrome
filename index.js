9b97620a652871e44ed84fa6cb65c764aafaba87
Moreen <moreencharles@gmail.com>
2023-10-29 17:54:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+  str = str.toLowerCase();
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+console.log(isPalindrome('abba')); // true
+console.log(isPalindrome('racecar')); // true
+console.log(isPalindrome('a')); // true
+console.log(isPalindrome('robot')); // false
+console.log(isPalindrome('ab')); // false
